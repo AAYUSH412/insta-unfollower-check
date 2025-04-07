543c6a3f4dbd3a4e8b288213ad3bb436b80408fd
Aayush <113169971+AAYUSH412@users.noreply.github.com>
2025-04-07 10:57:25 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
           <div className="flex flex-col md:flex-row md:items-center md:space-x-8 space-y-3 md:space-y-0">
             <div className="flex space-x-4 items-center">
               <a 
-                href="https://github.com/AAYUSH412/insta_unfollower_list" 
+                href="https://github.com/AAYUSH412" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 aria-label="GitHub Repository"
